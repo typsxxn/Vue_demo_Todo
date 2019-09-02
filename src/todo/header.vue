872745f9b6b11cacea3fcba7271be606131a9979
typsxxn <1076379096@qq.com>
2019-09-02 20:15:11 +0800

    vue_demo_Todo

---
+++ src/todo/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="main-header">
+        <h1>Todo</h1>
+    </header>
+</template>
+
+<style lang="stylus" scoped>
+.main-header{
+  text-align center
+  h1{
+    font-size 100px
+    color: #E8EAF6
+    font-weight 100
+    margin 50px
+  }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,130 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+    target: 'web',
+    // 声明入口
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'build.[hash:8].js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader'
+            },
+
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name]-xxn.[ext]'
+                        }
+                    }
+
+                ]
+            }
+        ]
+
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin(),
+        new VueLoaderPlugin()
+    ]
+};
+
+if (isDev) {
+    config.module.rules.push({
+        test: /\.styl/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true,
+                }
+            },
+            'stylus-loader'
+        ]
+    }, )
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+        port: '8000',
+        host: 'localhost',
+        overlay: {
+            erros: true,
+        },
+        open: true,
+        hot: true,
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.entry = {
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[chunkHash:8].css'),
+    );
+    config.optimization = {
+        splitChunks: {
+            cacheGroups: { // 这里开始设置缓存的 chunks
+                commons: {
+                    chunks: 'initial', // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    minSize: 0, // 最小尺寸，默认0,
+                    minChunks: 2, // 最小 chunk ，默认1
+                    maxInitialRequests: 5 // 最大初始化请求书，默认1
+                },
+                vendor: {
+                    test: /node_modules/, // 正则规则验证，如果符合就提取 chunk
+                    chunks: 'initial', // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    name: 'vendor', // 要缓存的 分隔出来的 chunk 名称
+                    priority: 10, // 缓存组优先级
+                    enforce: true
+                }
+            }
+        },
+        runtimeChunk: true
+    }
+
+}
+
+module.exports = config
